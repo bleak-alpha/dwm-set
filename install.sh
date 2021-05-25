5673039586d1fac12238447f43463b933edd5b9a
bleak-alpha <bleak-alpha@protonmail.ch>
2021-05-25 14:16:37 +0000

    main

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+printf "Installing and Setting Up DWM\n"
+cd Downloads/dwm-set/dwm-6.2/
+sudo make clean install
+cd .. && cd dmenu-5.0/
+sudo make clean install
+cd .. && cd slstatus/
+sudo make clean install
+cd .. && cd st-0.8.2/
+sudo make clean install
+
+#Laptop Touchpad restore 
+#cd ..
+#sudo cp 30-touchpad-Laptop-touchpad-conf.conf /etc/X11/xorg.conf.d/30-touchpad.conf
