9add0f3863fbca90e4ce26a3d2f360d3aed5152a
bleak-alpha <bleak-alpha@protonmail.ch>
2021-12-20 14:20:03 +0530

    restores dotfiles
    Signed-off-by: bleak-alpha <bleak-alpha@protonmail.ch>

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 printf "Intalling prerequisites....."
 
-sudo pacman -Syy --noconfirm xorg xorg-server xorg-xrandr xorg-xinit xorg-xsetroot arandr xbindkeys pavucontrol blueman firefox thunderbird dunst feh picom git wget curl wget neovim vim ttf-fira-code ttf-font-awesome otf-font-awesome powerline-fonts awesome-terminal-fonts lxappearance materia-gtk-theme xbindkeys playerctl pcmanfm deepin-screenshot otf-fira-mono noto-fonts-emoji gnu-free-fonts noto-fonts 
+sudo pacman -Syy --noconfirm xorg xorg-server xorg-xrandr xorg-xinit xorg-xsetroot arandr xbindkeys pavucontrol blueman firefox thunderbird dunst feh picom git wget curl wget neovim vim ttf-fira-code ttf-font-awesome otf-font-awesome powerline-fonts awesome-terminal-fonts lxappearance materia-gtk-theme xbindkeys playerctl pcmanfm deepin-screenshot otf-fira-mono noto-fonts-emoji gnu-free-fonts noto-fonts zsh 
 
 printf "Installing and Setting Up DWM\n"
 cd dwm-6.2/
@@ -19,3 +19,9 @@ sudo make clean install
 #Laptop Touchpad restore 
 #cd ..
 #sudo cp 30-touchpad-Laptop-touchpad-conf.conf /etc/X11/xorg.conf.d/30-touchpad.conf
+
+printf "Restoring dotfiles....."
+git clone https://github.com/bleak-alpha/dotfiles
+rm -fr dotfiles/backup.sh && rm -fr dotfiles/restore.sh
+cp dotfiles/* $'HOME'/
+rm -fr dotfiles/
